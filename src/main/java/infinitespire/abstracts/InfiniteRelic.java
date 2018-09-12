e621c488a23ad1d0b0412a6bcb58e138532a343b
Grayson Gullion <xinflux@gmail.com>
2018-09-11 21:31:36 -0400

    Add Shard Relics

---
+++ src/main/java/infinitespire/abstracts/InfiniteRelic.java
@@ -0,0 +1,25 @@
+package infinitespire.abstracts;
+
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rooms.MonsterRoomBoss;
+
+public abstract class InfiniteRelic extends Relic {
+    public InfiniteRelic(String setId, String textureID, RelicTier tier, LandingSound sfx) {
+        super(setId, textureID, tier, sfx);
+        this.counter = 1;
+        if(AbstractDungeon.player != null)
+            this.updateDescription(AbstractDungeon.player.chosenClass);
+    }
+
+    @Override
+    public void onVictory() {
+        if(AbstractDungeon.getCurrRoom() instanceof MonsterRoomBoss) {
+            this.counter ++;
+        }
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return DESCRIPTIONS[0] + "1" + DESCRIPTIONS[1];
+    }
+}
---
+++ src/main/java/infinitespire/relics/crystals/WardingShard.java
@@ -0,0 +1,25 @@
+package infinitespire.relics.crystals;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import infinitespire.InfiniteSpire;
+import infinitespire.abstracts.InfiniteRelic;
+
+public class WardingShard extends InfiniteRelic {
+
+    public static final String ID = InfiniteSpire.createID("WardingShard");
+
+    public WardingShard(){
+        super(ID, "wardingshard", RelicTier.SPECIAL, LandingSound.CLINK);
+    }
+
+    @Override
+    public void atBattleStart() {
+        AbstractDungeon.actionManager.addToBottom(
+            new ApplyPowerAction(
+                AbstractDungeon.player,
+                AbstractDungeon.player,
+                new DexterityPower(AbstractDungeon.player, counter), counter));
+    }
+}
