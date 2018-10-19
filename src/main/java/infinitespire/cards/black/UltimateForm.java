1686c86a287197b4c29d0c2673a52804add1e2cc
Grayson Gullion <xinflux@gmail.com>
2018-10-19 18:27:34 -0400

    New VFX for Ultimate Form

--- src/main/java/infinitespire/cards/black/UltimateForm.java
+++ src/main/java/infinitespire/cards/black/UltimateForm.java
@@ -1,11 +1,13 @@
 package infinitespire.cards.black;
 
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import infinitespire.abstracts.BlackCard;
+import infinitespire.effects.uniqueVFX.UltimateFormEffect;
 import infinitespire.powers.UltimateFormPower;
 
 public class UltimateForm extends BlackCard {
@@ -38,6 +40,7 @@ public class UltimateForm extends BlackCard {
 
 	@Override
 	public void useWithEffect(AbstractPlayer p, AbstractMonster m) {
+		AbstractDungeon.actionManager.addToBottom(new VFXAction(p, new UltimateFormEffect(p.hb.cX, p.hb.cY), 1.0f));
 		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p, new UltimateFormPower(p, this.magicNumber), this.magicNumber));
 	}
 }
