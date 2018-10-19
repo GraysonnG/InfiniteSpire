4f860fe35710b65b148afe3d34994bdd6d9768b2
Grayson Gullion <xinflux@gmail.com>
2018-10-19 01:09:07 -0400

    Change Version Number

--- src/main/java/infinitespire/InfiniteSpire.java
+++ src/main/java/infinitespire/InfiniteSpire.java
@@ -58,7 +58,7 @@ import java.util.ArrayList;
 @SpireInitializer
 public class InfiniteSpire implements PostInitializeSubscriber, PostBattleSubscriber, EditRelicsSubscriber,
 		EditCardsSubscriber, EditKeywordsSubscriber, EditStringsSubscriber, PreDungeonUpdateSubscriber {
-	public static final String VERSION = "0.2.4";
+	public static final String VERSION = "0.3.0";
 	public static final Logger logger = LogManager.getLogger(InfiniteSpire.class.getName());
 
 	private static ArrayList<OnQuestRemovedSubscriber> onQuestRemovedSubscribers = new ArrayList<>();
---
+++ src/main/java/infinitespire/cards/black/UNNAMED_1.java
@@ -0,0 +1,57 @@
+package infinitespire.cards.black;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.IntangiblePlayerPower;
+import com.megacrit.cardcrawl.relics.ChemicalX;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import infinitespire.abstracts.BlackCard;
+
+public class UNNAMED_1 extends BlackCard {
+
+	public static final String ID = "UNNAMED_1";
+	public static final String NAME = "Unnamed Card";
+	public static final String IMG = "img/infinitespire/cards/beta.png";
+	public static final String DESCRIPTION = "Gain X Intangible.";
+	public static final String DESCRIPTION_U = "Gain X + 1 Intangible.";
+
+	public static final int COST = -1;
+
+	public static final CardType TYPE = CardType.SKILL;
+	public static final CardTarget TARGET = CardTarget.SELF;
+
+	public UNNAMED_1(){
+		super(ID, NAME, IMG, COST, DESCRIPTION, TYPE, TARGET);
+	}
+
+
+	@Override
+	public void upgrade() {
+		if(!this.upgraded){
+			this.upgradeName();
+			this.rawDescription = DESCRIPTION_U;
+			this.initializeDescription();
+		}
+	}
+
+	@Override
+	public void use(AbstractPlayer p, AbstractMonster abstractMonster) {
+		if(this.energyOnUse < EnergyPanel.totalCount) {
+			this.energyOnUse = EnergyPanel.totalCount;
+		}
+
+		int energyAmount = this.energyOnUse;
+
+		if(p.hasRelic(ChemicalX.ID)) {
+			p.getRelic(ChemicalX.ID).flash();
+			energyAmount += 2;
+		}
+
+		if(this.upgraded) energyAmount += 1;
+
+		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p,
+			new IntangiblePlayerPower(p, energyAmount), energyAmount));
+	}
+}
