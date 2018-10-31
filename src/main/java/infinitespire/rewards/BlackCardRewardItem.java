e60cb74403918d7ac78a0d6e648cbba60a3124ce
Grayson Gullion <xinflux@gmail.com>
2018-10-31 17:35:32 -0400

    Make Lord of Annihilation Drop Black Card Reward

---
+++ src/main/java/infinitespire/rewards/BlackCardRewardItem.java
@@ -0,0 +1,28 @@
+package infinitespire.rewards;
+
+import basemod.abstracts.CustomReward;
+import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.rewards.RewardItem;
+import infinitespire.helpers.CardHelper;
+import infinitespire.util.TextureLoader;
+
+public class BlackCardRewardItem extends CustomReward {
+
+	private static final Texture TEXTURE = TextureLoader.getTexture("img/infinitespire/ui/cardReward/blackreward.png");
+	private static final String TEXT = "Add a Black Card to your deck";
+
+	public BlackCardRewardItem() {
+		super(TEXTURE, TEXT, RewardItem.RewardType.CARD);
+		this.cards = CardHelper.getBlackRewardCards();
+	}
+
+	@Override
+	public boolean claimReward() {
+		if(AbstractDungeon.screen == AbstractDungeon.CurrentScreen.COMBAT_REWARD) {
+			AbstractDungeon.cardRewardScreen.open(this.cards, this, "Pick A Black Card.");
+			AbstractDungeon.previousScreen = AbstractDungeon.CurrentScreen.COMBAT_REWARD;
+		}
+		return false;
+	}
+}
