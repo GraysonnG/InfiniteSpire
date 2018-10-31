555bbc117b259472d2c8b58c06a8d3f9787d3e39
Grayson Gullion <xinflux@gmail.com>
2018-10-31 17:34:54 -0400

    Fix Pacifist cost updating wrong.

--- src/main/java/infinitespire/cards/Pacifist.java
+++ src/main/java/infinitespire/cards/Pacifist.java
@@ -2,6 +2,7 @@ package infinitespire.cards;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.BlueCandle;
 import infinitespire.abstracts.Card;
@@ -46,7 +47,9 @@ public class Pacifist extends Card{
 
 	@Override
 	public void triggerWhenDrawn() {
-		this.cost = -2;
+		if (AbstractDungeon.player.hasRelic(BlueCandle.ID)) {
+			this.cost = -2;
+		}
 	}
 
 
