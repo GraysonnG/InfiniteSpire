609021c70e893aae69f5f570303be3b5248d3344
Grayson Gullion <xinflux@gmail.com>
2018-11-27 15:26:56 -0500

    Rework Burning Sword

--- src/main/java/infinitespire/relics/BurningSword.java
+++ src/main/java/infinitespire/relics/BurningSword.java
@@ -1,20 +1,24 @@
 package infinitespire.relics;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.status.Burn;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-
 import infinitespire.InfiniteSpire;
 import infinitespire.abstracts.Relic;
+import infinitespire.powers.CriticalPower;
 
 public class BurningSword extends Relic {
 
 	public static final String ID = InfiniteSpire.createID("Burning Sword");
+	public static int attacksPlayed = 0;
 	
 	public BurningSword() {
-		super(ID, "burningsword", RelicTier.COMMON, LandingSound.MAGICAL);
+		super(ID, "burningsword", RelicTier.BOSS, LandingSound.MAGICAL);
+		attacksPlayed = 0;
 	}
 
 	@Override
@@ -23,8 +27,22 @@ public class BurningSword extends Relic {
 	}
 
 	@Override
-	public void atBattleStart() {
-		AbstractPlayer p = AbstractDungeon.player;
-		AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(p, p, new StrengthPower(p, 2), 2));
+	public void atTurnStart() {
+		attacksPlayed = 0;
+		AbstractDungeon.actionManager.addToBottom(
+			new ApplyPowerAction(
+				AbstractDungeon.player,
+				AbstractDungeon.player,
+				new CriticalPower(AbstractDungeon.player), 1));
+	}
+
+	@Override
+	public void onPlayCard(AbstractCard c, AbstractMonster m) {
+		if(c.type == AbstractCard.CardType.ATTACK && attacksPlayed == 0){
+			AbstractCard burn = new Burn();
+			burn.upgrade();
+			AbstractDungeon.actionManager.addToBottom(new MakeTempCardInHandAction(burn));
+			attacksPlayed++;
+		}
 	}
 }
