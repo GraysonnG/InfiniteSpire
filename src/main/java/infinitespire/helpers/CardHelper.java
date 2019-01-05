7ce77cc46ab387e435c106d9ec646a5754022b57
Grayson Gullion <xinflux@gmail.com>
2019-01-05 14:06:33 -0500

    Fixes for 0.10.3

--- src/main/java/infinitespire/helpers/CardHelper.java
+++ src/main/java/infinitespire/helpers/CardHelper.java
@@ -46,14 +46,14 @@ public class CardHelper {
 		return cards;
 	}
 	
-	public static AbstractCard getRandomBlackCard() {
+	public static BlackCard getRandomBlackCard() {
 		BlackCard card =  blackCards.get(AbstractDungeon.cardRng.random(blackCards.size() -1));
 		UnlockTracker.markCardAsSeen(card.cardID);
 		card.isSeen = true;
 		if(InfiniteSpire.isHubrisLoaded){
 			HubrisCrossover.loadInfiniteBlow();
 		}
-		return card.makeStatEquivalentCopy();
+		return (BlackCard) card.makeStatEquivalentCopy();
 	}
 	
 	public static void addCard(AbstractCard card) {
