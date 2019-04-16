1b721599ef79553c81561cfa26b3a68c6411f2e7
Grayson Gullion <grayson.gullion@dynamit.com>
2019-04-16 17:15:17 -0400

    Update Version Number

--- src/main/java/infinitespire/InfiniteSpire.java
+++ src/main/java/infinitespire/InfiniteSpire.java
@@ -75,7 +75,7 @@ import java.util.ArrayList;
 @SpireInitializer
 public class InfiniteSpire implements PostInitializeSubscriber, PostBattleSubscriber, EditRelicsSubscriber,
 	EditCardsSubscriber, EditKeywordsSubscriber, EditStringsSubscriber, PreDungeonUpdateSubscriber, PostUpdateSubscriber {
-	public static final String VERSION = "0.16.0";
+	public static final String VERSION = "0.17.0";
 	public static final Logger logger = LogManager.getLogger(InfiniteSpire.class.getName());
 
 	private static ArrayList<OnQuestRemovedSubscriber> onQuestRemovedSubscribers = new ArrayList<>();
