bcce6baa30352d0606f2fa34ee6d82fd07cd80d9
Grayson Gullion <xinflux@gmail.com>
2020-04-26 18:58:59 -0400

    add sfx for obtaining void shards

--- src/main/kotlin/com/blanktheevil/infinitespire/extensions/InfiniteSpireExtensions.kt
+++ src/main/kotlin/com/blanktheevil/infinitespire/extensions/InfiniteSpireExtensions.kt
@@ -1,12 +1,14 @@
 package com.blanktheevil.infinitespire.extensions
 
 import com.blanktheevil.infinitespire.InfiniteSpire
+import com.megacrit.cardcrawl.core.CardCrawlGame
 import org.apache.logging.log4j.Logger
 import java.lang.Math.PI
 
 fun addVoidShard(count: Int) {
   InfiniteSpire.saveData.voidShards.count += count
   InfiniteSpire.voidShardDisplay.flash()
+  CardCrawlGame.sound.play("RELIC_DROP_MAGICAL")
 }
 
 fun Float.toRadians(): Float = this.times(PI.div(180f)).toFloat()
